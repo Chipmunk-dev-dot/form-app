515164f4d5a6040898b7d106be259af9ae49df76
Zanele Kunene <zanele.kunene@cdasolutions.co.za>
2025-08-19 16:16:08 +0200

    Create ButtonWrapper.js

---
+++ src/Components/FormsUI/Button/ButtonWrapper.js
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
+import { useFormikContext } from 'formik';
+
+const ButtonWrapper = ({children,...otherProps}) => {
+
+
+    const {submitForm}=useFormikContext();
+    const handleSubmit=()=>{
+        submitForm();
+      
+    }
+
+    const configButton={
+        variant: 'contained',
+        color: 'primary',
+        fullWidth: true,
+        onClick: handleSubmit
+    }
+
+
+
+
+  return (
+    <Button  {...configButton}>
+        {children}
+    </Button>
+  )
+}
+
+export default ButtonWrapper
